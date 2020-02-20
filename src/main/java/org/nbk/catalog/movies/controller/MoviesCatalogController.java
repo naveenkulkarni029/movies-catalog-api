369cf5d6a816a8a7ab5c42d35858ab07d43755f8
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2020-02-20 10:50:31 +0530

    removed unwanted imports

--- src/main/java/org/nbk/catalog/movies/controller/MoviesCatalogController.java
+++ src/main/java/org/nbk/catalog/movies/controller/MoviesCatalogController.java
@@ -2,12 +2,10 @@ package org.nbk.catalog.movies.controller;
 
 import org.nbk.catalog.movies.domain.Movie;
 import org.nbk.catalog.movies.domain.MovieCatalog;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MoviesCatalogController {
