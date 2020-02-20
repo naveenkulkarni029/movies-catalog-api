db34e779281636df4dcb6596450dc20f48f84d67
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2020-02-20 23:37:29 +0530

    added hystirx and fallback

--- src/main/java/org/nbk/catalog/movies/controller/MoviesCatalogController.java
+++ src/main/java/org/nbk/catalog/movies/controller/MoviesCatalogController.java
@@ -2,27 +2,37 @@ package org.nbk.catalog.movies.controller;
 
 import org.nbk.catalog.movies.domain.Movie;
 import org.nbk.catalog.movies.domain.MovieCatalog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class MoviesCatalogController {
 
-	/*@Autowired
-	private RestTemplate restTemplate;*/
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@GetMapping(value = "api/v1/movie-catalog/catalogs", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<MovieCatalog> getMovieCatalog() {
+    
+    @HystrixCommand(fallbackMethod = "fallback")
+    @GetMapping(value = "api/v1/movie-catalog/catalogs", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<MovieCatalog> getMovieCatalog() {
 
-		//Movie movie = restTemplate.getForObject("http://movies-api/api/v1/movies", Movie.class);
-		Movie movie =new Movie();
-		movie.setMovieName("3 idiots");
-		MovieCatalog catalog = new MovieCatalog();
-		catalog.setMovie(movie);
-		return ResponseEntity.ok(catalog);
-
-	}
+	Movie movie = restTemplate.getForObject("http://movies-api/api/v1/movies", Movie.class);
+	//Movie movie = new Movie();
+	movie.setMovieName("3 idiots");
+	MovieCatalog catalog = new MovieCatalog();
+	catalog.setMovie(movie);
+	return ResponseEntity.ok(catalog);
+    }
+    
+    public ResponseEntity<MovieCatalog> fallback(Throwable hystrixCommand){
+	return new ResponseEntity<>(new MovieCatalog(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
